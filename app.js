83cbe2b3a903e55c074beed0ffece56908189519
Eduardo ML <edu@eml.run>
2023-10-15 11:11:40 -0600

    nuevo endpoint con json

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('API Check-in Tlamabyte V2.0')
 })
 
+app.get('/data', (req, res) => {
+    res.json({"nombre": "Eduardo"})
+})
+
 app.listen(port, () => {
     console.log(`Servicio API se levantó correctamente en el puerto ${port}`)
 })
